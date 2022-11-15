992111006a4e39c1e6a32dca282d97029381ecf7
cra19a <cra19a@acu.edu>
2022-11-15 09:19:43 -0600

    basic dice working

--- Swift_D20_CRA/ViewController.swift
+++ Swift_D20_CRA/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImageView: UIImageView!
+    @IBOutlet var criticalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonGotPressed() {
+        rollDice()
+    }
+    
+    func rollDice() {
+        
+        print("They see me rollin... they hatin")
+        
+        let rolledNumber = Int.random(in: 1...20)
+        
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+        
+    }
 
 }
 
