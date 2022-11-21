cc82d2afe80bf61db226e6b2151a41f56a3ab43d
cra19a <cra19a@acu.edu>
2022-11-21 17:20:20 -0600

    final commit and push

--- Swift_D20_CRA/ViewController.swift
+++ Swift_D20_CRA/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet var diceImageView: UIImageView!
     @IBOutlet var criticalLabel: UILabel!
+    var audioPlayer: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,19 +21,75 @@ class ViewController: UIViewController {
 
     @IBAction func buttonGotPressed() {
         rollDice()
+        
+        
     }
     
     func rollDice() {
         
-        print("They see me rollin... they hatin")
+        // print("They see me rollin... they hatin")
         
         let rolledNumber = Int.random(in: 1...20)
-        
         let imageName = "d\(rolledNumber)"
-        
         diceImageView.image = UIImage(named: imageName)
         
+        switch imageName {
+        case ("d1"):
+            criticalLabel.isHidden = false
+            criticalLabel.text = "Critical Miss!"
+            let pathToSound = Bundle.main.path(forResource: "failwah", ofType: "mp3")!
+
+            let url = URL(fileURLWithPath: pathToSound)
+                    
+            do
+            {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+            }
+            catch
+            {
+                print("error babe")
+            }
+            
+        case ("d20"):
+            criticalLabel.isHidden = false
+            criticalLabel.text = "Critical Hit!"
+            let pathToSound = Bundle.main.path(forResource: "torpedo", ofType: "mp3")!
+
+            let url = URL(fileURLWithPath: pathToSound)
+                    
+            do
+            {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+            }
+            catch
+            {
+                print("error babe")
+            }
+            
+        default:
+            criticalLabel.isHidden = true
+            let pathToSound = Bundle.main.path(forResource: "rolldice", ofType: "mp3")!
+            
+            let url = URL(fileURLWithPath: pathToSound)
+            do
+            {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+            }
+            catch
+            {
+                print("error")
+            }
+        }
+        
+        // need sounds for rolling, crit hit, and crit miss
     }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDice()
+    }
+    
 }
 
